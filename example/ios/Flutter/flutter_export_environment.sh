976e171905c023b2b7a30edbd7f1f1d7ce6de1d2
David <david@theqoos.com>
2020-01-07 20:07:36 +0900

    init

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/david/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/david/myownproject/flutter_slidable/example"
+export "FLUTTER_TARGET=/Users/david/myownproject/flutter_slidable/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/david/flutter/bin/cache/artifacts/engine/ios"
